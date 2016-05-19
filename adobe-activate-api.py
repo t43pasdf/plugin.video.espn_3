51326bee40501b847f787b84fd46b573d9326702
anthony <anthony@centipede.gateway.pace.com>
2016-05-18 20:53:46 -0400

    Started updating the api auth to work within kodi

--- adobe-activate-api.py
+++ adobe-activate-api.py
@@ -10,14 +10,34 @@ import random
 import time
 import json
 import gzip
+import os
 from StringIO import StringIO
-from datetime import datetime
 
+from globals import ADDON_PATH_PROFILE
+
+SETTINGS_FILE = 'adobe.json'
 UA_ATV = 'AppleCoreMedia/1.0.0.13Y234 (Apple TV; U; CPU OS 9_2 like Mac OS X; en_us)'
-device_id = str(uuid.uuid1())
 
-# Get regcode test generateRegCode
-if False:
+def save_settings(settings):
+    settings_file = os.path.join(ADDON_PATH_PROFILE, SETTINGS_FILE)
+    with open(settings_file, 'w') as fp:
+        json.dump(settings, fp, sort_keys=True, indent=4)
+
+def load_settings():
+    settings_file = os.path.join(ADDON_PATH_PROFILE, SETTINGS_FILE)
+    if not os.path.isfile(settings_file):
+        save_settings(dict())
+    with open(settings_file, 'r') as fp:
+        return json.load(fp)
+
+def get_device_id():
+    settings = load_settings()
+    if 'device_id' not in settings:
+        settings['device_id'] = str(uuid.uuid1())
+        save_settings(settings)
+    return settings['device_id']
+
+def get_regcode():
     nonce = str(uuid.uuid4())
     params = urllib.urlencode(
         {'deviceId': device_id,
@@ -47,12 +67,13 @@ if False:
     resp = opener.open(url, urllib.urlencode(dict()))
     resp = resp.read()
     print resp
-    resp = json.loads(resp)
-    reg_code = resp['code']
 
-# TODO: Would need to store when reg code expires incase the user doesn't immediately
+# TODO: Would need to store when reg code expires in case the user doesn't immediately
 # activate the device
 # called espn.regCode
+resp = '{"id":"","code":"","requestor":"ESPN","generated":1463616806831,"expires":1463618606831,"info":{"deviceId":"","deviceType":"appletv","deviceUser":null,"appId":null,"appVersion":null,"registrationURL":null}}'
+resp = json.loads(resp)
+reg_code = resp['code']
 
 raw_input("Press Enter to continue...")
 
@@ -91,41 +112,123 @@ if False:
 
     print content
 
+content = '{"mvpd":"","requestor":"ESPN","userId":"","expires":"1466208969000"}'
 # Called espn.authn
+raw_input("Press Enter to continue...")
+
+if False:
+    # Get authn token (re-auth device after it expires), getAuthnToken
+    nonce = str(uuid.uuid4())
+    params = urllib.urlencode({'requestor': 'ESPN',
+                               'deviceId' : device_id})
+
+    url = urlparse.urlunsplit(['https',
+                                   'api.auth.adobe.com',
+                                   'api/v1/tokens/authn',
+                                   params, ''])
+
+    today = str(int(time.time() * 1000))
+    key = 'gB8HYdEPyezeYbR1'
+    message = 'GET requestor_id=ESPN, nonce=' + nonce + ', signature_method=HMAC-SHA1, request_time=' + today + ', request_uri=/tokens/authn'
+    signature = hmac.new(key, message, hashlib.sha1)
+    signature = base64.b64encode(signature.digest())
+    message = message + ', public_key=yKpsHYd8TOITdTMJHmkJOVmgbb2DykNK, signature=' + signature
+
+    opener = urllib2.build_opener()
+    opener.addheaders = [ ("Accept", "application/json"),
+                            ("Accept-Encoding", "gzip, deflate"),
+                            ("Accept-Language", "en-us"),
+                            ("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8"),
+                            ("Connection", "close"),
+                            ("User-Agent", UA_ATV),
+                            ("Authorization", message)]
+    resp = opener.open(url)
+    if resp.info().get('Content-Encoding') == 'gzip':
+        buf = StringIO(resp.read())
+        f = gzip.GzipFile(fileobj=buf)
+        content = f.read()
+    else:
+        content = resp.read()
+
+    print content
 
 
-# Get authn token
-nonce = str(uuid.uuid4())
-params = urllib.urlencode({'requestor': 'ESPN',
-                           'deviceId' : device_id})
+if False:
+    # authorize
+    nonce = str(uuid.uuid4())
+    params = urllib.urlencode({'requestor': 'ESPN',
+                               'deviceId' : device_id,
+                               'resource' : 'TODO resource'})
 
-url = urlparse.urlunsplit(['https',
-                               'api.auth.adobe.com',
-                               'api/v1/tokens/authn',
-                               params, ''])
+    url = urlparse.urlunsplit(['https',
+                                   'api.auth.adobe.com',
+                                   'api/v1/authorize',
+                                   params, ''])
 
-today = str(int(time.time() * 1000))
-key = 'gB8HYdEPyezeYbR1'
-message = 'GET requestor_id=ESPN, nonce=' + nonce + ', signature_method=HMAC-SHA1, request_time=' + today + ', request_uri=/tokens/authn'
-signature = hmac.new(key, message, hashlib.sha1)
-signature = base64.b64encode(signature.digest())
-message = message + ', public_key=yKpsHYd8TOITdTMJHmkJOVmgbb2DykNK, signature=' + signature
+    today = str(int(time.time() * 1000))
+    key = 'gB8HYdEPyezeYbR1'
+    message = 'GET requestor_id=ESPN, nonce=' + nonce + ', signature_method=HMAC-SHA1, request_time=' + today + ', request_uri=/authorize'
+    signature = hmac.new(key, message, hashlib.sha1)
+    signature = base64.b64encode(signature.digest())
+    message = message + ', public_key=yKpsHYd8TOITdTMJHmkJOVmgbb2DykNK, signature=' + signature
 
-opener = urllib2.build_opener()
-opener.addheaders = [ ("Accept", "application/json"),
-                        ("Accept-Encoding", "gzip, deflate"),
-                        ("Accept-Language", "en-us"),
-                        ("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8"),
-                        ("Connection", "close"),
-                        ("User-Agent", UA_ATV),
-                        ("Authorization", message)]
-resp = opener.open(url)
-if resp.info().get('Content-Encoding') == 'gzip':
-    buf = StringIO(resp.read())
-    f = gzip.GzipFile(fileobj=buf)
-    content = f.read()
-else:
-    content = resp.read()
+    opener = urllib2.build_opener()
+    opener.addheaders = [ ("Accept", "application/json"),
+                            ("Accept-Encoding", "gzip, deflate"),
+                            ("Accept-Language", "en-us"),
+                            ("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8"),
+                            ("Connection", "close"),
+                            ("User-Agent", UA_ATV),
+                            ("Authorization", message)]
+    resp = opener.open(url)
+    if resp.info().get('Content-Encoding') == 'gzip':
+        buf = StringIO(resp.read())
+        f = gzip.GzipFile(fileobj=buf)
+        content = f.read()
+    else:
+        content = resp.read()
 
-print content
+    print content
 
+content = '{"resource":"TODO resource","mvpd":"","requestor":"ESPN","expires":"1463621239000"}'
+
+if True:
+    # getShortMediaToken
+    nonce = str(uuid.uuid4())
+    params = urllib.urlencode({'requestor': 'ESPN',
+                               'deviceId' : device_id,
+                               'resource' : 'TODO resource'})
+
+    url = urlparse.urlunsplit(['https',
+                                   'api.auth.adobe.com',
+                                   'api/v1/mediatoken',
+                                   params, ''])
+
+    today = str(int(time.time() * 1000))
+    key = 'gB8HYdEPyezeYbR1'
+    message = 'GET requestor_id=ESPN, nonce=' + nonce + ', signature_method=HMAC-SHA1, request_time=' + today + ', request_uri=/mediatoken'
+    signature = hmac.new(key, message, hashlib.sha1)
+    signature = base64.b64encode(signature.digest())
+    message = message + ', public_key=yKpsHYd8TOITdTMJHmkJOVmgbb2DykNK, signature=' + signature
+
+    opener = urllib2.build_opener()
+    opener.addheaders = [ ("Accept", "application/json"),
+                            ("Accept-Encoding", "gzip, deflate"),
+                            ("Accept-Language", "en-us"),
+                            ("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8"),
+                            ("Connection", "close"),
+                            ("User-Agent", UA_ATV),
+                            ("Authorization", message)]
+    resp = opener.open(url)
+    if resp.info().get('Content-Encoding') == 'gzip':
+        buf = StringIO(resp.read())
+        f = gzip.GzipFile(fileobj=buf)
+        content = f.read()
+    else:
+        content = resp.read()
+
+    print content
+
+content = '{"mvpdId":"","expires":"1463618218000","serializedToken":"+++++++=","userId":"","requestor":"ESPN","resource":"TODO resource"}'
+
+# serializedtoken = pkan
