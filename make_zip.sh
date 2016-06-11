a5a46cffebdd5394b00f76a355a06a24a35baa50
awaters1 <awaters1@gmail.com>
2016-06-11 11:13:53 -0400

    Updated the make zip script

--- make_zip.sh
+++ make_zip.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-zip -r plugin.video.espn_3.zip * -x .git/\* -x bugs/\* -x \*.pyo -x \*.pyc
+DIR=plugin.video.espn_3
+cd ..
+zip -r plugin.video.espn_3.zip $DIR -x $DIR/.git/\* -x $DIR/bugs/\* -x $DIR/\*.pyo -x $DIR/\*.pyc -x $DIR/.idea/\*
 
