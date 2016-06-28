d7f34a30304b2736e7b9db180f9fcd6363aabb9e
awaters1 <awaters1@gmail.com>
2016-06-27 21:24:09 -0400

    Updated the script

--- make_repo-plugins.sh
+++ make_repo-plugins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR=plugin.video.espn_3
 cd ..
-rsync -av --copy-links --delete $DIRx repo-plugins/plugin.video.espn_3/ --exclude "*.pyo" --exclude "*.pyc" --exclude ".git" --exclude ".idea" --exclude "*.sh" --exclude "bugs" --exclude ".*" --exclude "TODO"
+rsync -av --copy-links --delete $DIR repo-plugins/ --exclude "*.pyo" --exclude "*.pyc" --exclude ".git" --exclude ".idea" --exclude "*.sh" --exclude "bugs" --exclude ".*" --exclude "TODO"
 
